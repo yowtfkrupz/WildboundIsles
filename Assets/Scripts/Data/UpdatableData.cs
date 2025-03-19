3d8f20c48979deca78e82fb43fba131dcd113b80
Tomáš Krupička <tomaskrupicka222@seznam.cz>
2025-03-19 09:32:01 +0100

    build error fix

--- Assets/Scripts/Data/UpdatableData.cs
+++ Assets/Scripts/Data/UpdatableData.cs
@@ -7,20 +7,58 @@ public class UpdatableData : ScriptableObject
     public event System.Action OnValuesUpdated;
     public bool autoUpdate;
 
+    private IUpdater updater;
+
+    protected virtual void OnEnable()
+    {
+#if UNITY_EDITOR
+        updater = new EditorUpdater();
+#else
+        updater = new BuildUpdater();
+#endif
+    }
+
     protected virtual void OnValidate()
     {
         if (autoUpdate)
         {
-            UnityEditor.EditorApplication.update += NotifyOfUpdatedValues;
+            updater.RegisterUpdateCallback(NotifyOfUpdatedValues);
         }
     }
 
     public void NotifyOfUpdatedValues()
     {
-        UnityEditor.EditorApplication.update -= NotifyOfUpdatedValues;
+        updater.UnregisterUpdateCallback(NotifyOfUpdatedValues);
         if (OnValuesUpdated != null)
         {
             OnValuesUpdated();
         }
+    }
+
+    private interface IUpdater
+    {
+        void RegisterUpdateCallback(System.Action callback);
+        void UnregisterUpdateCallback(System.Action callback);
+    }
+
+#if UNITY_EDITOR
+    private class EditorUpdater : IUpdater
+    {
+        public void RegisterUpdateCallback(System.Action callback)
+        {
+            UnityEditor.EditorApplication.update += () => callback();
+        }
+
+        public void UnregisterUpdateCallback(System.Action callback)
+        {
+            UnityEditor.EditorApplication.update -= () => callback();
+        }
+    }
+#endif
+
+    private class BuildUpdater : IUpdater
+    {
+        public void RegisterUpdateCallback(System.Action callback) { }
+        public void UnregisterUpdateCallback(System.Action callback) { }
     }
 }
